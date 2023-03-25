3455a738b9f9e458be69e94112bf659202742e34
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-03-25 18:15:26 +0000

    added styling and functionalities to add pokemons to MongoDB and remove them from cart list

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,11 +2,13 @@ import axios from "axios";
 import { useState } from "react";
 import pokeball from "../images/pokeball.png";
 
+import "../styles/homepage.css";
+
 export const HomePage = () => {
   const [inputPokemon, setInputPokemon] = useState("");
   const [visibleData, setVisibleData] = useState("");
   const [pokemonCart, setPokemonCart] = useState([]);
-  const [error, setError] = useState(false);
+  const [error, setError] = useState(null);
 
   const showPokemonCard = async () => {
     try {
@@ -14,25 +16,31 @@ export const HomePage = () => {
         `http://localhost:3001/pokemons/${inputPokemon}`
       );
       setVisibleData(response.data);
+      console.log(visibleData.id);
     } catch (error) {
-      setError(true);
       console.log(error);
     }
   };
 
   const handleAddPokemon = async () => {
     const response = await axios.post(`http://localhost:3001/pokemons`, {
-        name: visibleData.name,
-        image: visibleData.image,
-      });
+      id: visibleData.id,
+      name: visibleData.name,
+      image: visibleData.image,
+    });
 
-      const newPokemon= JSON.parse(response.config.data);
+    const newPokemon = JSON.parse(response.config.data);
 
-      setPokemonCart([
-        ...pokemonCart,
-        newPokemon,
-      ]);
-    console.log(response);
+    setPokemonCart([...pokemonCart, newPokemon]);
+    console.log(newPokemon);
+    setInputPokemon("");
+  };
+
+  const handleDeletePokemon = async (id) => {
+    const startedIndex = pokemonCart.findIndex((pokemon) => pokemon.id !== id);
+    const newPokemonCart = [...pokemonCart].splice(startedIndex, 1);
+
+    setPokemonCart(newPokemonCart);
   };
 
   return (
@@ -56,10 +64,11 @@ export const HomePage = () => {
       </div>
 
       <div className="data-container">
+        <div className="empty-div"></div>
         {visibleData && (
           <div className="flex place-items-center m-8">
             <span>{visibleData.name}</span>
-            <img className="mx-4" src={visibleData.image} alt="pokemon pic"></img>
+            <img className="mx-4" src={visibleData.image} alt="pokemon pic" />
             <button type="submit" onClick={handleAddPokemon}>
               Add to your cart
             </button>
@@ -67,9 +76,24 @@ export const HomePage = () => {
         )}
         {/*!error && visibleData === undefined && <div>Sorry, this Pokemon is not available</div>*/}
 
-        <div className="cart-container flex flex-col">
-            <h3 className="flex flex-col items-left">Your Pokedex <img src={pokeball} alt="pokeball-icon" className="w-1/5"></img></h3>
-            {pokemonCart.map(creature => <div className="flex"><img src={creature.image} alt="pokemon saved pic"/> <p className="place-items-center">{creature.name}</p></div>)}
+        <div className="cart-container flex flex-col m-2">
+          <h3 className="flex items-left font-bold border border-4 rounded-lg border-red-500 w-fit p-4">
+            Your Pokedex{" "}
+            <img src={pokeball} alt="pokeball-icon" className="pokeball mx-2" />
+          </h3>
+          {pokemonCart.map((creature, index) => (
+            <div key={index} className="flex items-center">
+              <img src={creature.image} alt="pokemon saved pic" />
+              <p>{creature.name}</p>
+              <button
+                type="delete"
+                className="delete-creature-btn mx-4 p-2 border border-2 rounded-lg border-red-500"
+                /*onClick={() => handleDeletePokemon(creature.id)}*/
+              >
+                Delete
+              </button>
+            </div>
+          ))}
         </div>
       </div>
     </div>
